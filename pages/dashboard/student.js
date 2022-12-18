c456cbb857ab31d25aa3414070454580549e71ef
Vivek Kumar <vivekkr.ind@gmail.com>
2022-12-18 19:19:06 +0530

    student half done

--- pages/dashboard/student.js
+++ pages/dashboard/student.js
@@ -3,50 +3,96 @@ import { useCallback } from 'react'
 import { useFormState } from 'react-hook-form';
 import SideNav from "../../components/SideNav";
 import Nav from "../../components/Nav";
+import Head from 'next/head';
+import Image from 'next/image'
 
 
 function student_dashboard() {
     const router = useRouter()
-    
+    const name = "vivek kumar";
+
+    const course = [
+        {
+            name: "Data Engineering",
+            instructor: "Mridula Verma",
+            type: "Elective",
+            semester: 7,
+            course_url: "https://api.multiavatar.com/check.png"
+        },
+        {
+            name: "Essentials of AI",
+            instructor: "Salman abdul",
+            type: "Core",
+            semester: 7,
+            course_url: "https://api.multiavatar.com/check.png"
+        }
+    ]
+
+    const totalCourse = [
+        { type: "Current Courses", count: 10, color: "decoration-blue-300" },
+        { type: "Course completed", count: 2, color: "decoration-green-300" }
+    ]
+
     return (
-        <div className='w-screen'>
+        <div>
+            <Head>
+                <title>Student Dashboard</title>
+            </Head>
             <div className="fixed">
-                <SideNav/>
+                <SideNav />
             </div>
             <div className="fixed">
-                <Nav/>
-            </div>
-                <div className='flex'>
-        
-            <div className='flex-auto w-2/4 h-screen mx-10'>
-                <div className="h-10">
-                    <p>Welcome, Vivek Kumar</p>
-                </div>
-                <div className="card dashboard-card" role="listitem" data-region="course-content">
-                    
-                    <div className="card-body">
-                    <img src="/home/vivek/security/react/student_resource_mgt/components/Ballnewypose.svg"/>
-                        <h5 className="card-title d-inline">Data Engineering</h5> 
-                        <div className="card-text">
+                <Nav />
+
+                <div className="pl-28 tracking-wide px-8 pt-6 text-matty-900">
+                    <h1 className="font-['Poppins'] text-3xl font-medium mb-4">Hello, {name}</h1>
+                    <div>
+                        <div className="grid grid-cols-2 gap-2">
+                                {totalCourse.map((list) => (
+                                    <div className="grid grid-cols-2 text-sm py-4 text-matty-600">
+                                        <div className="col-span-2">{list.type}</div>
+                                        <h1 className={`text-4xl underline underline-offset-8 decoration-8 ${list.color} font-['Poppins'] font-semibold`}>{list.count}</h1>
+                                    </div>
+                                ))}
+
+                        </div>
+                        <div className="grid grid-cols-4 gap-4">
+                            <div className="col-span-2">
+                                <div className="grid grid-cols-5 text-sm py-4 text-matty-600">
+                                    <div className="col-span-2">Course Title</div>
+                                    <div className="">Semester</div>
+                                    <div className="">Type</div>
+                                    <div className="">Instructor</div>
+                                </div>
+                                {course.map((list) => (
+                                    <div key={list.name} className="grid grid-cols-5 py-3">
+                                        <div className="flex col-span-2 gap-3">
+                                            {list.name}
+                                        </div>
+                                        <div className="flex gap-4">
+                                            {list.semester}
+                                        </div>
+                                        <div className="flex gap-3">
+                                            {list.type}
+                                        </div>
+                                        <div className="flex gap-3">
+                                            <Image src={list.course_url} width={24} height={24} alt={list.name} />
+                                            {list.instructor}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
 
                         </div>
                     </div>
-                    
+
+
+
                 </div>
-
-
             </div>
 
-            <div className='flex-auto bg-comp2 w-2/4 h-screen '>
-                
-            </div>
         </div>
-        </div>
-        
-
-
 
     )
 }
-
 export default student_dashboard
